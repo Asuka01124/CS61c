2ea0edf91258a3bf67121b8047fc0f2a0e679a86
asuka <ziliny175@gmail.com>
2026-05-30 02:07:14 +0800

    lab03

--- labs/lab03/ex5_factorial.s
+++ labs/lab03/ex5_factorial.s
@@ -27,7 +27,14 @@ main:
 # The return value should be stored in a0
 factorial:
     # YOUR CODE HERE
-
+    mv t1 a0
+    li a0 1
+loop:
+    beq t1 x0 done
+    mul a0 a0 t1
+    addi t1 t1 -1 
+    j loop
     # This is how you return from a function. You'll learn more about this later.
     # This should be the last line in your program.
+done:
     jr ra
